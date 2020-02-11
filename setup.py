499364b596d77e7d314755d30652478404dae112
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-02-11 14:32:04 +0100

    Packaging

---
+++ setup.py
@@ -0,0 +1,27 @@
+"""tug-overlap-checker - setup.py"""
+import setuptools
+
+LONG_DESC = open('README.md').read()
+
+setuptools.setup(
+    name="tug-overlap-checker",
+    version="0.1.1",
+    author="Lorenz Leitner",
+    author_email="lrnz.ltnr@gmail.com",
+    description="Check for course overlaps at Graz University of Technology",
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESC,
+    url="https://github.com/lolei/tug-overlap-checker",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["tug_overlap_checker"],
+    entry_points={"console_scripts": ["tug-overlap-checker=tug_overlap_checker.tug_overlap_checker:main"]},
+    python_requires=">=3",
+    install_requires=[
+        'bs4',
+        'selenium'
+    ]
+    )
